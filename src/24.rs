4b58c22137d4f3f834064ec04f6fbad6b8c1d99d
noldenthompkins685 <noldenthompkins685@gmail.com>
2025-04-03 13:47:32 +0000

    Add 24.rs

---
+++ src/24.rs
@@ -0,0 +1,7 @@
+fn main() {
+    // Example Rust code for mathematical operations
+    let num1 = 5;
+    let num2 = 3;
+    let sum: i32 = num1 + num2;
+    println!("The sum of {} and {} is {}", num1, num2, sum);
+}
