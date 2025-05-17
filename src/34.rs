ff3507a5ff67286e95928ae020f5e9374bcbff65
noldenthompkins685 <noldenthompkins685@gmail.com>
2025-05-17 04:11:52 +0000

    Add 34.rs

---
+++ src/34.rs
@@ -0,0 +1,16 @@
+// Example Rust code demonstrating a simple loop and conditional statements
+
+fn main() {
+    let number = 5;
+
+    // Loop from 1 to 10
+    for i in 1..=10 {
+        println!("{}", i);
+    }
+
+    if number > 3 {
+        println!("Number is greater than 3");
+    } else {
+        println!("Number is not greater than 3");
+    }
+}
